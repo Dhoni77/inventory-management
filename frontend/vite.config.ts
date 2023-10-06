e3729b159863d3407a5dd4db1c101a8de7e0d624
Dhoni77 <aldrinmarz7@gmail.com>
2023-10-06 21:40:06 +0530

    chore: frontend config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,25 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => `${env.VITE_BASE_URL}${path}`
+        }
+      }
+    }
+  }
 })
