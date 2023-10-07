2119a8bb0168c8fd3430662e7049f8fe8a35e33d
Dhoni77 <aldrinmarz7@gmail.com>
2023-10-07 11:34:26 +0530

    chore: remove console.log

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 export async function connect() {
-    console.log(process.env.DATABASE_URL)
     await mongoose.connect(process.env.DATABASE_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
